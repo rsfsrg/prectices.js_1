7e688c5bbbb06474d8435430ae06dab05fd4d381
secret valley <88777641+rsfsrg@users.noreply.github.com>
2024-07-26 12:36:19 +0530

    Add files via upload

---
+++ prac.js
@@ -0,0 +1,76 @@
+// const profile = {
+
+//     name: "shardahKapoor",
+//     post:195,
+//     follower: BigInt(569),
+//     following:4,
+//     follow:true,
+// }
+
+// let a, b;
+// a = 2;
+// b = 3;
+// console.log("a + b =", a + b);
+// console.log("a - b =", a - b);
+// console.log("a * b =", a * b);
+// console.log("a / b =", a / b);
+// console.log("a % b =", a % b);
+// console.log("a ** b =", a ** b);
+// uniary operator
+
+// let a, b;
+// a = 2;
+// b = 3;
+// console.log("a + b =", a + b);
+
+// a = a + 1;
+
+// console.log(a);
+
+// let x, type;
+// x = 566;
+
+// if(x % 2 === 0){
+//     console.log(x, " is even");
+// } else{
+//     console.log(x, "is odd");
+// }
+
+// let multi = prompt("Enter a NUmber:");
+
+// if(multi % 5 === 0 ){
+//     console.log( multi,"is themultiple of 5");
+// } else{
+//     console.log( multi,"is not a multiple of 5");
+// }
+
+let grade = 78;
+ let score;
+
+
+if(grade >= 80 && grade <= 100){
+
+    score = " A";
+
+} else if(grade >= 70 && grade <= 89){
+
+    score = " B";
+
+} else if(grade >= 60 && grade <= 69){
+
+    score = " C";
+
+}else if(grade >= 50 && grade <= 59){
+    
+    score = " D";
+
+} else if( grade = 0 && grade <= 49){
+
+    score = "y F";
+
+} else{
+    score = "NoT DEFINED";
+};
+
+console.log("according to your score your grade was ", score);
+
